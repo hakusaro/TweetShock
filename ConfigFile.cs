d85dbdade9c426283fc45434a66895f467caec18
Lucas Nicodemus <nicatron21@gmail.com>
2012-01-21 03:47:54 -0700

    Initial commit

---
+++ ConfigFile.cs
@@ -0,0 +1,45 @@
+﻿using System;
+using System.Collections.Generic;
+using System.IO;
+using System.Linq;
+using System.Text;
+using Newtonsoft.Json;
+
+namespace TweetShock
+{
+	class ConfigFile
+	{
+		public string path = Path.Combine("tshock", "tweetshock.json");
+
+		public ConfigLayout ReadConfigFile()
+		{
+			if (File.Exists(path))
+			{
+				TextReader tr = new StreamReader(path);
+				string pre = tr.ReadToEnd();
+				tr.Close();
+				return JsonConvert.DeserializeObject<ConfigLayout>(pre);
+			}
+			return null;
+		}
+
+		public void WriteConfigFile()
+		{
+			if (!File.Exists(path))
+			{
+				TextWriter tw = new StreamWriter(path);
+				string json = JsonConvert.SerializeObject(new ConfigLayout());
+				tw.Write(json);
+				tw.Close();
+			}
+		}
+	}
+
+	class ConfigLayout
+	{
+		public string AccessToken = "";
+		public string AccessTokenSecret = "";
+		public string ConsumerKey = "";
+		public string ConsumerSecret = "";
+	}
+}
---
+++ TweetShock.cs
@@ -0,0 +1,79 @@
+﻿using System;
+using System.Collections.Generic;
+using System.ComponentModel;
+using System.Linq;
+using System.Text;
+using TShockAPI;
+using Terraria;
+using Twitterizer;
+
+namespace TweetShock
+{
+	[APIVersion(1,11)]
+	public class TweetShock : TerrariaPlugin
+	{
+		public TweetShock(Main game) : base(game)
+		{
+			Order = 100;
+		}
+
+		private OAuthTokens tokens = new OAuthTokens();
+		private ConfigFile config = new ConfigFile();
+		private ConfigLayout configFile = new ConfigLayout();
+		public override Version Version
+		{
+			get { return new Version(1, 0); }
+		}
+
+		public override string Author
+		{
+			get { return "nicatronTg"; }
+		}
+
+		public override string Name
+		{
+			get { return "TweetShock"; }
+		}
+
+		public override string Description
+		{
+			get { return "Because if it doesn't run Twitter, we don't care."; }
+		}
+
+		public override void Initialize()
+		{
+			config.WriteConfigFile();
+			configFile = config.ReadConfigFile();
+			Console.WriteLine("Welcome to TweetShock. Initial configuration read OK. Verify that you've set all app keys before continuing.");
+
+			tokens.AccessToken = configFile.AccessToken;
+			tokens.AccessTokenSecret = configFile.AccessTokenSecret;
+			tokens.ConsumerKey = configFile.ConsumerKey;
+			tokens.ConsumerSecret = configFile.ConsumerSecret;
+
+			Hooks.ServerHooks.Connect += TweetJoin;
+		}
+
+		public void TweetJoin(int id, HandledEventArgs args)
+		{
+			// SendTweet("Player " + Main.player[id].name + " joined the server.");
+			Console.WriteLine("Player: " + Main.player[id].name);
+		}
+
+		public bool SendTweet(string msg)
+		{
+			TwitterResponse<TwitterStatus> tw = TwitterStatus.Update(tokens, msg);
+			if (tw.Result == RequestResult.Success)
+			{
+				Console.WriteLine("Twitter: Tweet posted successfully.");
+				return true;
+			}
+			else
+			{
+				Console.WriteLine("Twitter: Something went wrong: ");
+				Console.WriteLine(tw.ErrorMessage);
+				return false;
+			}
+		}
+	}
+}
